9af69e3799aa29068f0b682796156f541b4a4e1d
GeoffLo <geoff.lo@yahoo.com>
2023-07-21 15:56:30 +0800

    Update settings Page

---
+++ app/src/main/java/cs/hku/wetrade/settingsActivity.java
@@ -0,0 +1,13 @@
+package cs.hku.wetrade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class settingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

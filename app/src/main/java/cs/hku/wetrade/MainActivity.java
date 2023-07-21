470e8348d348b962290f0d64b44c74569ee87413
Rainy Wu <wuyu@auralabs.com.hk>
2023-07-21 18:35:10 +0800

    add hku,666 as test account

--- app/src/main/java/cs/hku/wetrade/MainActivity.java
+++ app/src/main/java/cs/hku/wetrade/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         if (user.equals("") || pwd.equals("")) {
             Toast.makeText(this, "The username or password cannot be null!", Toast.LENGTH_SHORT).show();
         }
-        if (user.equals("hku") || pwd.equals("666")) {
+        if (user.equals("hku") && pwd.equals("666")) {
             Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);

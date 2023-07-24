d711e1dbe08d621b83b6c723f2229b26118133e8
GeoffLo <geoff.lo@yahoo.com>
2023-07-25 00:53:57 +0800

    Update Settings Page

--- app/src/main/java/cs/hku/wetrade/SettingActivity.java
+++ app/src/main/java/cs/hku/wetrade/SettingActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -49,6 +52,26 @@ public class SettingActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+
+        ImageView home = findViewById(R.id.home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SettingActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button logout = findViewById(R.id.Logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }

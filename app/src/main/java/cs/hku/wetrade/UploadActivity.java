bbdd707bec571ae25c3e3088a5a7f372c76189a1
Rainy Wu <wuyu@auralabs.com.hk>
2023-07-26 00:19:16 +0800

    connect to phone album

--- app/src/main/java/cs/hku/wetrade/UploadActivity.java
+++ app/src/main/java/cs/hku/wetrade/UploadActivity.java
@@ -1,11 +1,24 @@
 package cs.hku.wetrade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,22 +31,29 @@ import java.io.ByteArrayOutputStream;
 public class UploadActivity extends AppCompatActivity {
     private Spinner spinner;
     Button uploadImage;
+    String[] mPermissionList = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+    public static final int REQUEST_PICK_IMAGE = 11101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload);
         initView();
 
-        uploadImage =(Button) findViewById(R.id.UploadPhotosButton);
+        uploadImage = (Button) findViewById(R.id.UploadPhotosButton);
 
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ActivityCompat.requestPermissions(UploadActivity.this, mPermissionList, 100);
 //                bitmapToString();
             }
         });
 
     }
+
     private void initView() {
         spinner = (Spinner) findViewById(R.id.spinner);
 
@@ -49,6 +69,112 @@ public class UploadActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Category cannot be null!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // Open phone album
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100:
+                boolean writeExternalStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readExternalStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (grantResults.length != 0 && writeExternalStorage && readExternalStorage) {
+                    getImage();
+                } else {
+                    Toast.makeText(this, "Please set the necessary permissions", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+    private void getImage() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            startActivityForResult(new Intent(Intent.ACTION_GET_CONTENT).setType("image/*"),
+                    REQUEST_PICK_IMAGE);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(intent, REQUEST_PICK_IMAGE);
+        }
+    }
+
+    // Gets the Uri returned by the album
+    @SuppressLint("NewApi")
+    private static String getRealPathFromUri(Context context, Uri uri) {
+        String filePath = null;
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // 如果是document类型的 uri, 则通过document id来进行处理
+            String documentId = DocumentsContract.getDocumentId(uri);
+            if (isMediaDocument(uri)) { // MediaProvider
+            // 使用':'分割
+                String id = documentId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = {id};
+                filePath = getDataColumn(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection, selectionArgs);
+            } else if (isDownloadsDocument(uri)) { // DownloadsProvider
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(documentId));
+                filePath = getDataColumn(context, contentUri, null, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是 content 类型的 Uri
+            filePath = getDataColumn(context, uri, null, null);
+        } else if ("file".equals(uri.getScheme())) {
+            // 如果是 file 类型的 Uri,直接获取图片对应的路径
+            filePath = uri.getPath();
+        }
+        return filePath;
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        String path = null;
+        String[] projection = new String[]{MediaStore.Images.Media.DATA};
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
+                path = cursor.getString(columnIndex);
+            }
+        } catch (Exception e) {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return path;
+    }
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is MediaProvider
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is DownloadsProvider
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_PICK_IMAGE:
+                    if (data != null) {
+                        String realPathFromUri = UploadActivity.getRealPathFromUri(this, data.getData());
+                    } else {
+                        Toast.makeText(this, "图片损坏，请重新选择", Toast.LENGTH_SHORT).show();
+                    }
+
+                    break;
+            }
+        }
     }
 
     // 把filePath地址对应的图片转换成Bitmap，然后再将bitmap转换成Base64字符串String
@@ -79,13 +205,13 @@ public class UploadActivity extends AppCompatActivity {
     }
 
     //计算图片的缩放质量
-    public static int calculateInSampleSize(BitmapFactory.Options options,int reqWidth, int reqHeight) {
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
 
         if (height > reqHeight || width > reqWidth) {
-            final int heightRatio = Math.round((float) height/ (float) reqHeight);
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
             final int widthRatio = Math.round((float) width / (float) reqWidth);
             inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
         }

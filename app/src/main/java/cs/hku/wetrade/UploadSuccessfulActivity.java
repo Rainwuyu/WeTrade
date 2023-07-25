f22607f5346237c98bbd84002da54f2359f92f2a
GeoffLo <geoff.lo@yahoo.com>
2023-07-25 19:40:40 +0800

    Connect upload successful page to app

--- app/src/main/java/cs/hku/wetrade/UploadSuccessfulActivity.java
+++ app/src/main/java/cs/hku/wetrade/UploadSuccessfulActivity.java
@@ -2,7 +2,10 @@ package cs.hku.wetrade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UploadSuccessfulActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class UploadSuccessfulActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uploadsuccessful);
+
+        Button OK = findViewById(R.id.OK);
+        OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UploadSuccessfulActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

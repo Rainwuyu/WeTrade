63580fba187701f372e538815606dbe2d3b5ae7b
GeoffLo <geoff.lo@yahoo.com>
2023-07-26 23:38:33 +0800

    Update settings page

--- app/src/main/java/cs/hku/wetrade/TransactionHistoryActivity.java
+++ app/src/main/java/cs/hku/wetrade/TransactionHistoryActivity.java
@@ -10,5 +10,8 @@ public class TransactionHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transactionhistory);
+
+
+
     }
 }

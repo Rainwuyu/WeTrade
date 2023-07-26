98363f3a8c451a427665b146f7095e3bd1478617
GeoffLo <geoff.lo@yahoo.com>
2023-07-27 01:25:02 +0800

    Update basket and basket activity

--- app/src/main/java/cs/hku/wetrade/BasketActivity.java
+++ app/src/main/java/cs/hku/wetrade/BasketActivity.java
@@ -2,7 +2,11 @@ package cs.hku.wetrade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class BasketActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class BasketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.basket);
+
+        Button confirm = findViewById(R.id.confirmButton);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BasketActivity.this, TransactionCompletedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView home = findViewById(R.id.home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BasketActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
